ae4830e230a3e4b74416867bf77955096f1efa9a
Adel <adeloabdallah1995@gmail.com>
2019-11-18 19:03:09 -0500

    Fixed stupid import based test failure

--- scanner/core.py
+++ scanner/core.py
@@ -2,28 +2,9 @@ try:
     from PIL import Image
 except ImportError:
     import Image
+from . import utils
 import pytesseract
 import os
-import cv2
-
-
-def binarizeImage(pathToImage):
-    """
-    Processes image with thresholding and blurring
-    """
-
-    image = cv2.imread(pathToImage)
-    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # thresholding to preprocess image
-    # useful for reading dark text over grey shapes
-    grey = cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    # blur to remove noise
-    grey = cv2.medianBlur(grey, 3)
-
-    filename = "temp.png"
-    cv2.imwrite(filename, grey)
 
 
 def ocrProcessing(pathToImage):
@@ -33,7 +14,7 @@ def ocrProcessing(pathToImage):
     # ya it's hardcoded, need a prettier solution
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
 
-    binarizeImage(pathToImage)
+    utils.binarizeImage(pathToImage)
     text = pytesseract.image_to_string(Image.open("temp.png"))
     os.remove("temp.png")
 
